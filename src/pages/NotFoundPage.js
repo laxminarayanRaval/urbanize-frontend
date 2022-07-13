4df2e605e9c23fb4c58f740dbd229dd33c3cf231
Laxminarayan Raval <ln.raval01@gmail.com>
2022-07-13 16:26:23 +0530

    404 Gif Added

--- src/pages/NotFoundPage.js
+++ src/pages/NotFoundPage.js
@@ -2,18 +2,28 @@ import React from "react";
 
 // import PageNotFoundAnimation from "../assets/lottiefiles/page_notfound_anim.json";
 // import Lottie from "lottie-react";
+import NotFoundGif from "../assets/gifs/404_error_with_landscape.gif";
 
 import { Box, Button } from "@mui/material";
 
 const NotFoundPage = () => {
   return (
-    <Box display="flex" justifyContent="center" alignItem="center">
-      {/* <Lottie
-        animationData={PageNotFoundAnimation}
-        style={{ width: "75%" }}
-        loop={true}
-      /> */} 404 | Page Not Found Animation Should be Here Sooner
-      <Button color="primary" href="/">Home</Button>
+    <Box
+      sx={{
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        mt: -5,
+      }}
+    >
+      <img
+        src={NotFoundGif}
+        style={{ maxWidth: '100%', height: "auto" }}
+        alt="404 | Page Not Found Animation Should be Here Sooner"
+      />
+      <Button color="primary" href="/">
+        Home
+      </Button>
     </Box>
   );
 };
