59f806907888cbbc2705ddd0c526a1ba0a197db2
Laxminarayan Raval <ln.raval01@gmail.com>
2022-07-20 11:55:48 +0530

    bug removed

--- src/component/Services.jsx
+++ src/component/Services.jsx
@@ -17,21 +17,112 @@ import ServiceListCard from "./ServiceListCard";
 
 import NoDataGif from "../assets/gifs/No_data.gif";
 
+const SubServicesTabs = ({
+  currServices = null,
+  selectedSubServiceId = null,
+  clickHandler,
+  ...props
+}) => {
+  const isSelected = (id) => selectedSubServiceId === id;
+  return (
+    <Grid
+      item
+      sx={{
+        display: "flex",
+        overflowX: "auto",
+        justifyContent: "flex-start",
+        p: 1,
+        mt: "-3vh",
+      }}
+    >
+      {!currServices &&
+        [1, 2, 3, 4].map((ele) => (
+          <Stack display="flex" key={ele} alignItems="center">
+            <Skeleton
+              variant="circle"
+              sx={{ width: "100px", height: "100px", borderRadius: 10 }}
+            />
+            <Skeleton
+              variant="text"
+              animation="wave"
+              sx={{ width: "100px", height: "60px", m: 1, borderRadius: 1 }}
+            />
+          </Stack>
+        ))}
+      {currServices?.subservice_set?.map((subService) => (
+        <Grid
+          className={makeSlug(subService.service_name)}
+          id={subService.id}
+          component={Paper}
+          // component={Link}
+          // href={`/services/${makeSlug(currServices?.service_name)}/${makeSlug(service.service_name)}/`}
+          elevation={isSelected(subService.id) ? 10 : 2}
+          xs={12}
+          sm={12}
+          md={12}
+          item
+          key={subService.id + "" + makeSlug(subService.service_name)}
+          sx={{
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+            justifyContent: "center",
+            borderRadius: 1,
+            padding: 1,
+            margin: 1,
+            cursor: "pointer",
+          }}
+          onClick={(event) => {
+            // clickHandler(event);
+            window.history.pushState(
+              null,
+              `${subService.service_name} | ${currServices?.service_name}`,
+              `/services/${makeSlug(currServices?.service_name)}/${makeSlug(
+                subService.service_name
+              )}/`
+            );
+            window.location.href = `/services/${makeSlug(
+              currServices?.service_name
+            )}/${makeSlug(subService.service_name)}/`;
+          }}
+        >
+          <Box
+            id={subService.id}
+            sx={{
+              backgroundImage: `url(${subService.img_url})`,
+              backgroundSize: "cover",
+              backgroundPosition: "repeat",
+              width: "100px",
+              height: "100px",
+              borderRadius: 10,
+              minWidth: "max-content",
+            }}
+          ></Box>
+          <Typography component="h3" variant="h6" id={subService.id}>
+            {subService.service_name}
+          </Typography>
+        </Grid>
+      ))}
+    </Grid>
+  );
+};
+
 const Services = () => {
   const [selectedSubServiceId, setSelectedSubServiceId] = useState(0);
   const services = useSelector((state) => state?.content?.services);
   // const subservices = useSelector((state) => state?.content?.subservices);
 
   const { service_name, subservice_name } = useParams();
-  const CurrentService = services?.find(
-    (ele) => ele.service_name === revertSlug(service_name)
-  );
+  const CurrentService =
+    services?.find((ele) => ele.service_name === revertSlug(service_name)) ??
+    services?.find((ele, index) => index === 0);
 
   // console.log("service_name", service_name, "subservice_name", subservice_name);
 
-  const CurrentSubService = CurrentService?.subservice_set?.find(
-    (ele) => ele.service_name === revertSlug(subservice_name)
-  );
+  const CurrentSubService =
+    CurrentService?.subservice_set?.find(
+      (ele) => ele.service_name === revertSlug(subservice_name)
+    ) ?? CurrentService?.subservice_set?.find((ele, index) => index === 0);
 
   const AvailableProfessionals =
     CurrentService?.professionaluserservice_set?.filter((ele) =>
@@ -70,94 +161,18 @@ const Services = () => {
     }
   }, [CurrentSubService]);
 
-  const clickHandler = (event) => {
+  const subServiceChangeHandler = (event) => {
     if (event.target.id === selectedSubServiceId) setSelectedSubServiceId(0);
     else setSelectedSubServiceId(event.target.id);
   };
 
-  const isSelected = (id) => selectedSubServiceId === id;
-
   return (
     <Grid container display="flex" justifyContent="center">
-      <Grid
-        item
-        sx={{
-          display: "flex",
-          overflowX: "auto",
-          justifyContent: "flex-start",
-          p: 1,
-          mt: "-3vh",
-        }}
-      >
-        {!CurrentService &&
-          [1, 2, 3, 4].map((ele) => (
-            <Stack display="flex" key={ele} alignItems="center">
-              <Skeleton
-                variant="circle"
-                sx={{ width: "100px", height: "100px", borderRadius: 10 }}
-              />
-              <Skeleton
-                variant="text"
-                animation="wave"
-                sx={{ width: "100px", height: "60px", m: 1, borderRadius: 1 }}
-              />
-            </Stack>
-          ))}
-        {CurrentService?.subservice_set?.map((service) => (
-          <Grid
-            className={makeSlug(service.service_name)}
-            id={service.id}
-            component={Paper}
-            // component={Link}
-            // href={`/services/${makeSlug(CurrentService?.service_name)}/${makeSlug(service.service_name)}/`}
-            elevation={isSelected(service.id) ? 10 : 2}
-            xs={12}
-            sm={12}
-            md={12}
-            item
-            key={service.id}
-            sx={{
-              display: "flex",
-              flexDirection: "column",
-              alignItems: "center",
-              justifyContent: "center",
-              borderRadius: 1,
-              padding: 1,
-              margin: 1,
-              cursor: "pointer",
-            }}
-            onClick={(event) => {
-              // clickHandler(event);
-              window.history.pushState(
-                null,
-                `${service.service_name} | ${CurrentService?.service_name}`,
-                `/services/${makeSlug(CurrentService?.service_name)}/${makeSlug(
-                  service.service_name
-                )}/`
-              );
-              window.location.href = `/services/${makeSlug(
-                CurrentService?.service_name
-              )}/${makeSlug(service.service_name)}/`;
-            }}
-          >
-            <Box
-              id={service.id}
-              sx={{
-                backgroundImage: `url(${service.img_url})`,
-                backgroundSize: "cover",
-                backgroundPosition: "repeat",
-                width: "100px",
-                height: "100px",
-                borderRadius: 10,
-                minWidth: "max-content",
-              }}
-            ></Box>
-            <Typography component="h3" variant="h6" id={service.id}>
-              {service.service_name}
-            </Typography>
-          </Grid>
-        ))}
-      </Grid>
+      <SubServicesTabs
+        currServices={CurrentService}
+        selectedSubServiceId={selectedSubServiceId}
+        clickHandler={subServiceChangeHandler}
+      />
       {!selectedSubServiceId == 0 && (
         <Grid container sx={{ width: "85%", mt: 2 }} item>
           <Grid
